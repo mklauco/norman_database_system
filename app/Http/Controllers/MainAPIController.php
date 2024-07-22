69bc3f03a7a37e539376fba7a71d12b2dba122cd
Martin Klauco <martin@klauco.com>
2024-07-22 19:49:28 +0200

    token plain text

--- app/Http/Controllers/MainAPIController.php
+++ app/Http/Controllers/MainAPIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class MainAPIController extends Controller
@@ -14,7 +15,7 @@ class MainAPIController extends Controller
 
         $user = User::find(Auth::id());
 
-        dd($user->tokens);
+        // dd($user->tokens);
         return view('apiresources.index', [
             'user' => $user,
         ]);
@@ -25,7 +26,11 @@ class MainAPIController extends Controller
             'token_name' => 'required',
         ]);
         $token = $request->user()->createToken($request->token_name);
-        // dd($token->plainTextToken);
+        // dd($token->accessToken->id);
+        // eloquent queiry to update the plaintexttoken to the database based on token id
+        DB::table('personal_access_tokens')->where('id', $token->accessToken->id)->update([ 'plain_text_token' => $token->plainTextToken ]);
+        
+        // dd($token);
         // 3|SlFIgwo8XBEbIYf6Xu0B39Hj4arD7SmSZBss5j1d98bd76c9
         return redirect()->route('apiresources.index');
     }
