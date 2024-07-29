de15c359047ecb9189bb4780ca68d667d8529fa8
Martin Klauco <martin.klauco@stuba.sk>
2024-07-29 15:51:55 +0200

    testing emails

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,14 @@
+<!-- resources/views/emails/test.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Email</title>
+</head>
+<body>
+    <h1>Test Email</h1>
+    <p>{{ $messageContent }}</p>
+</body>
+</html>
