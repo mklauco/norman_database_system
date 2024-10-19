de0c4748265cf254fb2f787983292d38b42144ba
Martin Klauco <martin@klauco.com>
2024-10-19 21:10:25 +0200

    minor updates

--- resources/views/empodat/filter.blade.php
+++ resources/views/empodat/filter.blade.php
@@ -4,32 +4,62 @@
   </x-slot>
   
   <div class="py-4">
-    <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-prose mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
+      
+      <div class="p-6 text-gray-900">
         
-        <div class="p-6 text-gray-900">
-          
-          
-          <!-- Main Search form -->
-          <div class="grid grid-cols-3 gap-5">
-            
-            <div id="searchAccordingToCategoryForm">
-              <div class="bg-gray-50 p-2">
-                
-              </div>
+        
+        
+        <!-- Main Search form -->
+        <div class="grid grid-cols-1 gap-5">
+        
+        <div id="searchOptions">
+          <div class="bg-gray-100 p-2">
+            <div class="font-bold mb-2">
+              Search options:
+            </div>
+            <div class="flex items-center space-x-4">
+              <label class="inline-flex items-center">
+                <input type="radio" class="form-radio text-indigo-600" name="searchOption" value="option1">
+                <span class="ml-2"><strong>AND</strong> condition to all criteria</span>
+              </label>
+              <label class="inline-flex items-center">
+                <input type="radio" class="form-radio text-indigo-600" name="searchOption" value="option2">
+                <span class="ml-2"><strong>OR</strong> conditions to all criteria</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div id="searchGeography">
+          <div class="bg-gray-100 p-2">
+            <div class="font-bold mb-2">
+              Geography criteria:
             </div>
             
-            
-            
-            
-            
-                          
-            
-            <!-- Main Search form -->
-            
-          </div>        
+          </div>
         </div>
+
+        <div id="searchSubstance">
+          <div class="bg-gray-100 p-2">
+            <div class="font-bold mb-2">
+              Substance criteria:
+            </div>
+            
+          </div>
+        </div>
+        
+        
+        
+        
+        
+                
+        
+        <!-- Main Search form -->
+        
+        </div>        
+      </div>
       </div>
     </div>
   </x-app-layout>
-  
