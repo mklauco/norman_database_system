133cf348fe86158055c565880858b4a0fc7e14bc
Martin Klauco <martin@klauco.com>
2024-11-17 19:20:29 +0100

    test cron

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('test:cron')->daily();
+Schedule::command('test:cron')->everyTenSeconds();
