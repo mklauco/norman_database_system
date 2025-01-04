795433c08668b2214e2b517066398640cf0e7472
Martin Klauco <martin@klauco.com>
2025-01-04 20:25:26 +0100

    main search finished

--- resources/views/empodat/filter.blade.php
+++ resources/views/empodat/filter.blade.php
@@ -7,16 +7,16 @@
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-lg rounded-0">
         
-        {!! dump($request) !!}
+        {{-- {!! dump($request) !!} --}}
         <!-- Main Search form -->
         <form  name="searchEmpodat" id="searchEmpodat" action="{{route('codsearch.search')}}" method="GET">
           
-          <div class="p-4 text-gray-900 grid grid-cols-1 gap-4">
+          <div class="px-4 text-gray-900 grid grid-cols-1 gap-4">
             <!-- Main Search form -->
             <form  name="searchEmpodat" id="searchEmpodat" action="{{route('codsearch.search')}}" method="GET">
               <div class="grid grid-cols-1 gap-5">
                 
-                <div id="searchOptions" class="pointer-events-none opacity-50">
+                {{-- <div id="searchOptions" class="pointer-events-none opacity-50">
                   <div class="bg-gray-100 p-2">
                     <div class="font-bold mb-2">
                       Search options:
@@ -32,7 +32,7 @@
                       </label>
                     </div>
                   </div>
-                </div>
+                </div> --}}
                 
               </div>
               
@@ -119,7 +119,7 @@
               <div id="searchSource" class="">
                 <div class="bg-gray-100 p-2">
                   <div class="font-bold mb-2">
-                    Source criteria:
+                    SLE Source criteria:
                   </div>
                   <div class="w-full">
                     @include('_t.form-apline-multiselect', [
@@ -168,8 +168,18 @@
                 </div>
               </div>
               
-              <div>
-                SEARCH based on: list_concentration_indicators
+              <div id="concentrationIndicatorSearch" class="">
+                <div class="flex bg-gray-100 p-2">
+                  <div class="w-full">
+                    <div class="font-bold mb-2">
+                      Concetration Indicators:
+                    </div>
+                    @include('_t.form-apline-multiselect', [
+                    'tag' => 'concentrationIndicatorSearch', 'list' => $concentrationIndicatorList,
+                    'active_ids' => isset($request->concentrationIndicatorSearch) ? $request->concentrationIndicatorSearch : [],
+                    ])
+                  </div>
+                </div>
               </div>
 
               <div id="typeDataSourcesSearch" class="">
@@ -184,27 +194,28 @@
                     ])
                   </div>
                   
-                  <div class="w-full pointer-events-none opacity-50">
+                  <div class="w-full">
                     <div class="font-bold mb-2">
                       Organisation:
                     </div>
                     @include('_t.form-apline-multiselect', [
-                    'tag' => 'organisationList', 'list' => $matrixList,
-                    'active_ids' => isset($request->matrixSearch) ? $request->matrixSearch : [],
+                    'tag' => 'dataSourceOrganisationSearch', 'list' => $dataSourceOrganisationList,
+                    'active_ids' => isset($request->dataSourceOrganisationSearch) ? $request->dataSourceOrganisationSearch : [],
                     ])
                   </div>
                 </div>
               </div>
               
-              <div id="searchLaboratory" class="pointer-events-none opacity-50">
+              <div id="dataSourceLaboratorySearch" class="">
                 <div class="flex bg-gray-100 p-2">
                   <div class="w-full">
                     <div class="font-bold mb-2">
                       Laboratory:
                     </div>
+                    {{-- {{ var_dump($dataSourceLaboratoryList) }} --}}
                     @include('_t.form-apline-multiselect', [
-                    'tag' => 'laboratoryList', 'list' => $sourceList,
-                    'active_ids' => isset($request->matrixSearch) ? $request->matrixSearch : [],
+                    'tag' => 'dataSourceLaboratorySearch', 'list' => $dataSourceLaboratoryList,
+                    'active_ids' => isset($request->dataSourceLaboratorySearch) ? $request->dataSourceLaboratorySearch : [],
                     ])
                   </div>
                 </div>
@@ -229,31 +240,29 @@
                 </div>
               </div> --}}
               
-              <div id="searchAnalyticaMethod" class="pointer-events-none opacity-50">
+              <div id="analyticalMethodSearch" class="">
                 <div class="flex bg-gray-100 p-2">
                   <div class="w-full">
                     <div class="font-bold mb-2">
                       Analytical method:
-                      TABLE: list_analytical_methods
                     </div>
                     @include('_t.form-apline-multiselect', [
-                    'tag' => 'analyticalMethodList', 'list' => $sourceList,
-                    'active_ids' => isset($request->matrixSearch) ? $request->matrixSearch : [],
+                    'tag' => 'analyticalMethodSearch', 'list' => $analyticalMethodsList,
+                    'active_ids' => isset($request->analyticalMethodSearch) ? $request->analyticalMethodSearch : [],
                     ])
                   </div>
                 </div>
               </div>
               
-              <div id="searchQualityInformationCategory" class="pointer-events-none opacity-50">
+              <div id="qualityAnalyticalMethodsSearch" class="">
                 <div class="flex bg-gray-100 p-2">
                   <div class="w-full">
                     <div class="font-bold mb-2">
                       Quality information category:
-                      TABLE: list_quality_empodat_analytical_methods
                     </div>
                     @include('_t.form-apline-multiselect', [
-                    'tag' => 'qualityInformationCategoryList', 'list' => $sourceList,
-                    'active_ids' => isset($request->matrixSearch) ? $request->matrixSearch : [],
+                    'tag' => 'qualityAnalyticalMethodsSearch', 'list' => $qualityAnalyticalMethodsList,
+                    'active_ids' => isset($request->qualityAnalyticalMethodsSearch) ? $request->qualityAnalyticalMethodsSearch : [],
                     ])
                   </div>
                 </div>
--- resources/views/livewire/empodat/query-counter.blade.php
+++ resources/views/livewire/empodat/query-counter.blade.php
@@ -6,7 +6,7 @@
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
             </svg>
-        </div>
+        </div>     
 
         {{-- Show the result once it's ready --}}
         <div wire:loading.remove>
