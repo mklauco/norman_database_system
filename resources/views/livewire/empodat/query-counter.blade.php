cadc4bb69bee04d694ab2c4359f801f7754d7ce7
Martin Klauco <martin@klauco.com>
2025-01-05 13:13:08 +0100

    minor updates

--- resources/views/livewire/empodat/query-counter.blade.php
+++ resources/views/livewire/empodat/query-counter.blade.php
@@ -1,6 +1,9 @@
 <div>
-    <div wire:init="init">
+    <div wire:init="init"  class="flex">
         {{-- Show loading spinner while the countResult is being calculated --}}
+        <div  class="py-2">
+            Number of matched records:
+        </div>
         <div wire:loading>
             <div class="flex items-center space-x-2">
                 <svg class="w-4 h-4 text-gray-500 animate-spin" style="animation-duration: 1s;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -11,23 +14,32 @@
             </div>
         </div>
 
+        
+        
         {{-- Show the result and the download button once ready --}}
-        <div wire:loading.remove>
+        <div wire:loading.remove  class="py-2 mx-1 font-bold">
             @if (is_numeric($countResult))
-                <p class="text-gray-800 font-bold mb-2">
-                    {{ number_format($countResult, 0, " ", " ") }}
-                </p>
-
-                {{-- Download CSV Button
-                <button 
-                    wire:click="downloadCsv" 
-                    class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                >
-                    Download IDs as CSV
-                </button> --}}
+            {{ number_format($countResult, 0, " ", " ") }}
             @else
-                <p class="text-red-600">{{ $countResult }}</p>
+            {{ $countResult }}
             @endif
         </div>
+
+        <div class="py-2"> 
+            of <span> {{number_format($empodatsCount, 0, " ", " ") }}</span> 
+        </div>        
+
+        <div class="ml-1">
+            @if(auth()->check())
+            <button wire:click="downloadCsv" class="btn-download py-2 px-2">
+                Download IDs as CSV
+            </button>
+            @else
+            <button class="py-2 px-2 pointer-events-none opacity-60">
+                Downloads are available for registered users only.
+            </button>
+            @endif
+        </div>
+        
     </div>
 </div>
