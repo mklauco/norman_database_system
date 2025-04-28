bd43422d12aeaef1bb588a9d2a3d02a1308e82bc
Martin Klauco <martin@klauco.com>
2025-04-28 14:01:57 +0200

    prioritisation modelling danube

---
+++ app/Models/Prioritisation/ModellingDanube.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Prioritisation;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModellingDanube extends Model
+{
+    //
+    protected $table = 'prioritisation_modelling_danube';
+}
