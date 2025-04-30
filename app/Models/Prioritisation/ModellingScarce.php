4bdb18dbadb13d267b8f71e0c71db6ec37b7d047
Martin Klauco <martin@klauco.com>
2025-04-30 20:21:29 +0200

    prioritisation module

--- app/Models/Prioritisation/ModellingScarce.php
+++ app/Models/Prioritisation/ModellingScarce.php
@@ -5,10 +5,11 @@ namespace App\Models\Prioritisation;
 use App\Models\Susdat\Substance;
 use Illuminate\Database\Eloquent\Model;
 
-class ModellingDanube extends Model
+class ModellingScarce extends Model
 {
     //
-    protected $table = 'prioritisation_modelling_danube';
+    protected $table = 'prioritisation_modelling_scarce';
+
 
     public function substance()
     {
